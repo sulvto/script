b802dc4d81118fc67da7e8a91295632383c4bc0b
sulvto <sulvto@email.com>
2019-06-01 13:06:57 +0800

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+
+
+# install on-my-zsh now
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+yum install autojump-zsh -y
+yum install shadowsocks-qt5 -y
+yum install flameshot -y
+yum install filezilla -y
+yum install bochs -y
+yum install tmux -y
+yum install nasm -y
+yum install wireshark -y
+yum install libreoffice -y
+yum install git -y
